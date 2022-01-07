8f05490ff12d12264781caf492b038ee68dfc13a
five <fivecc@qq.com>
2022-01-07 18:03:28 +0800

    file

---
+++ zt/test/js/swiper-init.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".swiper-container",{slidesPerView:3,spaceBetween:0,centeredSlides:!0,loop:!0,loopedSlides:0,pagination:{el:".swiper-pagination",clickable:!0,bulletClass:"my_bullet",bulletActiveClass:"my_bullet_active"},autoplay:{disableOnInteraction:!1,delay:1500,pauseOnMouseEnter:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
